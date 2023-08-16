c26da92dfb4e35f60ea0f4168950aa379d7da69d
Millan Wang <real.millan.wang@gmail.com>
2023-08-15 21:44:28 -0400

    Removed sidethread approach

--- Limidi_Desktop/Services/MidiEventSender.cs
+++ Limidi_Desktop/Services/MidiEventSender.cs
@@ -2,19 +2,13 @@
 
 public class MidiEventSender : IMidiEventSender
 {
-    private static int OUTPUT_OPEN_DURATION_MILLISECONDS = 10000;
-
-
     private int _selectedDeviceID;
     private IMidiOutput? _currentOutput;
-    private DateTime _mostRecentRequestTime;
-
 
     public MidiEventSender()
     {
         _selectedDeviceID = 1;
         _currentOutput = null;
-        _mostRecentRequestTime = DateTime.Now;
     }
 
     public IEnumerable<String> GetAllInputDeviceNames()
@@ -35,7 +29,7 @@ public class MidiEventSender : IMidiEventSender
 
     public bool SetCurrentInputDevice(int deviceID)
     {
-        if (deviceID < 0 || deviceID >= MidiAccessManager.Default.Outputs.ToList<IMidiPortDetails>().Count())
+        if (deviceID < 0 || deviceID >= MidiAccessManager.Default.Outputs.ToList().Count())
         {
             return false;
         }
@@ -64,45 +58,24 @@ public class MidiEventSender : IMidiEventSender
 
     private void _sendMidiSignal(byte eventType, byte index, byte amount)
     {
-        _mostRecentRequestTime = DateTime.Now;
         if (_currentOutput == null)
         {
             this._currentOutput = MidiAccessManager.Default.OpenOutputAsync(this._selectedDeviceID.ToString()).Result;
-            _createOutputCloserThread();
         }
         _currentOutput.Send(
-            new byte[] { eventType, index, amount },
-            0, // Offset
-            3, // Length. Always 3 bytes
-            0 // Timestamp
-        );
+                new byte[] { eventType, index, amount },
+                0, // Offset
+                3, // Length. Always 3 bytes
+                0 // Timestamp
+            );
     }
 
-    private void _createOutputCloserThread()
-    {
-        (new Thread(() => { OutputCloserSideThread(this); })).Start();
-    }
 
     public void CloseOutput()
     {
         if (this._currentOutput == null) return; // Do nothing if already closed
-        if (DateTime.Now.Subtract(this._mostRecentRequestTime).TotalMilliseconds <= OUTPUT_OPEN_DURATION_MILLISECONDS)
-        { // The time until close was reset
-            Console.WriteLine("Previous close thread jumped. Make new one");
-            _createOutputCloserThread();
-            return;
-        }
-        // TODO - Validate the close process and put locks around usage of _current output
-        Console.WriteLine("Closed output @ " + DateTime.Now);
-        this._currentOutput?.CloseAsync();
+        this._currentOutput.CloseAsync();
         this._currentOutput = null;
-    }
-
-    static void OutputCloserSideThread(MidiEventSender midiEventSender)
-    {
-        //Sleep expects milliseconds
-        Thread.Sleep(OUTPUT_OPEN_DURATION_MILLISECONDS);
-        midiEventSender.CloseOutput();
     }
 }
 
